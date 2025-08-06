8a43c311d56baaf063be9124744b292c02c8dd6f
Udaykumar-2025 <udayashok1997@gmail.com>
2025-08-06 23:00:56 +0530

    Update App.tsx
    reverted the rote addition

--- src/App.tsx
+++ src/App.tsx
@@ -128,8 +128,7 @@ function AppContent() {
         <Route path="/" element={!user ? <Login /> : <Navigate to="/home" />} />
         <Route path="/provider/login" element={!user ? <ProviderLogin /> : <Navigate to="/provider/dashboard" />} />
         <Route path="/provider/signup" element={!user ? <ProviderSignup /> : <Navigate to="/provider/dashboard" />} />
-        <Route path="/provider/dashboard" element={<ProviderDashboard />} />
-        <Route path="/home" element={<home />} />
+      
         
         {/* Admin Routes */}
         <Route path="/admin/login" element={<AdminLogin />} />

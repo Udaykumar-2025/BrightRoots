cb801a60a103b418b25a574342554f92c51a8a31
Udaykumar-2025 <udayashok1997@gmail.com>
2025-08-09 14:20:20 +0530

    Added route for providerdetails

--- src/App.tsx
+++ src/App.tsx
@@ -129,6 +129,8 @@ function AppContent() {
         <Route path="/provider/login" element={!user ? <ProviderLogin /> : <Navigate to="/provider/dashboard" />} />
         <Route path="/provider/signup" element={!user ? <ProviderSignup /> : <Navigate to="/provider/dashboard" />} />
         <Route path="/provider/dashboard" element={<ProviderDashboard />} />
+        <Route path="/provider/:id" element={<ProviderDetails />} />
+
         
         {/* Admin Routes */}
         <Route path="/admin/login" element={<AdminLogin />} />
